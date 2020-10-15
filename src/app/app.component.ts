ae2d096a2d374b3f81b3005228608aacd774ab53
Adam Alexandru <adamalexandru4@yahoo.com>
2020-10-15 11:17:32 +0300

    Finishing demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-clone';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://google.ro/', 1),
+      new Article('Angular 2', 'http://facebook.com', 2),
+      new Article('Angular 3', 'http://sts.ro', 3)
+    ]
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = ``;
+    link.value  = ``;
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((art1: Article, art2: Article) => art2.votes - art1.votes);
+  }
+
 }
